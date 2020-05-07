49e679992f61f84e9537cb6c6c3d6ec8e589ec8d
rizkygin <rizkygin@gmail.com>
2020-05-07 12:17:25 +0700

    updated

--- app/src/main/java/com/example/tourguide/Activity/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/tourguide/Activity/ui/home/HomeFragment.java
@@ -1,17 +1,21 @@
 package com.example.tourguide.Activity.ui.home;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -26,6 +30,8 @@ import com.example.tourguide.model.DataMerchant;
 import com.example.tourguide.model.MerchantIndex;
 import com.example.tourguide.service.Api;
 import com.example.tourguide.service.UserClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -33,7 +39,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.IOException;
 import java.net.SocketTimeoutException;
@@ -51,11 +59,19 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "LandingMainActivity";
     private HomeViewModel homeViewModel;
     private RecyclerView recyclerView;
+    Integer REQUEST_LOCATION_PERMISSION = 2;
+
 
     String token;
+    FusedLocationProviderClient mFusedLocationClient;
+    Location mLastLocation;
+
     private GoogleMap mMap;
-    List<Recommended> mList;
-    CoordinatorLayout collapse;
+    private boolean isReady = false;
+    List<Recommended> mList = new ArrayList<>();
+    private final List<Marker> mMarker = new ArrayList<Marker>();
+
+    private ProgressBar progressBar;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -68,16 +84,16 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
         recyclerView = root.findViewById(R.id.recyclerView);
-        collapse = root.findViewById(R.id.recomendedPlaces);
+        progressBar = root.findViewById(R.id.progressBar);
 
+        progressBar.setVisibility(View.GONE);
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
 
-
+        getLocation();
+        callApi();
         return root;
-
-
     }
 
 
@@ -86,40 +102,111 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
 
         SharedPreferences preferences = getContext().getSharedPreferences("UserData", Context.MODE_PRIVATE);
-
-
         token = preferences.getString("token", "");
-        Log.d(TAG, "Bearer " + token);
+
+        Log.d(TAG, "size mList OnCreate " + mList.size());
+        Log.d(TAG, "isReady " + isReady);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        Log.d(TAG, "isReady " + isReady);
+
+        googleMap.setTrafficEnabled(true);
+        float zoomLevel = 16.0f;
+        mMap = googleMap;
+
+
+//            addMarkersToMap();
+//        mList.add(new Recommended("hello","there","0"));
+//        Log.d(TAG, String.valueOf(mList.size()));
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney")
+//                .icon(null));
+
+
+//        Log.d(TAG,"Name " +mList.get(30).getName()+ " "+ mList.size());
+//        mMap.addMarker(new MarkerOptions()
+//                .position(sydney)
+//                        .title(mList.get(1).getName())
+//                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.bag)));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
+
+
+    }
+
+//    private void addMarkersToMap() {
+//        if(mList.size() != 0 ){
+//            return;
+//        }
+//        else {
+//            callApi();
+//            addMarkersToMap();
+//        }
+//    }
+
+    //    private void addMarkersToMap() {
+//        if(mList.size() != 0){
+//            for (int i = 0; i < mList.size(); i++) {
+//                mMap.addMarker(new MarkerOptions()
+//                        .position(new LatLng(Double.parseDouble(mList.get(i).getLatitude()),Double.parseDouble(mList.get(i).getLongitude())))
+//                        .title(mList.get(i).getName())
+//                        .icon(BitmapDescriptorFactory.defaultMarker(R.drawable.shop_marker)));
+//
+//            }
+//
+//        }
+//        else if (mList.size() == 0){
+//            callApi();
+////            Log.d(TAG,"berulang Kali ini cuk");
+//            Log.d(TAG,"Size : " + mList.size());
+////            addMarkersToMap();
+//        }
+//    }
+    private void callApi() {
         Call<MerchantIndex> call = Api.getClient().recomendedplace("Bearer " + token);
-
-
         call.enqueue(new Callback<MerchantIndex>() {
             @Override
             public void onResponse(Call<MerchantIndex> call, Response<MerchantIndex> response) {
+                progressBar.setVisibility(View.VISIBLE);
+
                 Log.d(TAG, response.message());
                 if (!response.isSuccessful()) {
                     Log.d(TAG, "Code :" + response.code());
                     return;
                 }
-                mList = new ArrayList<>();
                 List<Recommended> list = response.body().getData();
                 for (Recommended recommended : list) {
+                    LatLng position = new LatLng(Double.parseDouble(recommended.getLatitude()), Double.parseDouble(recommended.getLongitude()));
 
 //                    Log.d(TAG, recommended.getName() + " " + recommended.getAddress() + " ");
-                    mList.add(new Recommended(recommended.getName(),recommended.getAddress(),recommended.getStatus()));
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(recommended.getLatitude()), Double.parseDouble(recommended.getLongitude()))).title(recommended.getName())
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.bag)));
+                    mList.add(new Recommended(recommended.getName(), recommended.getAddress(), recommended.getStatus()));
+                    Marker marker = mMap.addMarker(new MarkerOptions()
+                            .position(position).title(recommended.getName())
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.shop_marker))
+                            .title(recommended.getName()));
+                    mMarker.add(marker);
+
                 }
+                Log.d(TAG, "Data " + mList);
+
 
                 RecyclerViewRecommendedAdapter recyclerViewRecommendedAdapter = new RecyclerViewRecommendedAdapter(getContext(), mList);
                 recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
                 recyclerView.setAdapter(recyclerViewRecommendedAdapter);
-                Log.d(TAG, "get Name :" + mList.get(1).getName());
-                Log.d(TAG, response.message() + " Code :" + response.code());
+                progressBar.setVisibility(View.GONE);
+                Log.d(TAG, "responses :" + mList.size());
+
             }
 
             @Override
             public void onFailure(Call<MerchantIndex> call, Throwable t) {
+                progressBar.setVisibility(View.VISIBLE);
+
                 String Message = t.getMessage();
                 Log.d(TAG + "f", Message);
 //                mList.add(new Recommended("Grand Hotel Swiss Berlin", "Jalan farming, No 11,Kec.Seminyek", "1"));
@@ -140,24 +227,33 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
                 }
             }
         });
+
+        isReady = true;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.setTrafficEnabled(true);
-        mMap = googleMap;
+    public void getLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                mLastLocation = location;
 
-//        mList.add(new Recommended("hello","there","0"));
-//        Log.d(TAG, String.valueOf(mList.size()));
+                                mAccuracy.setText("Accuracy : " + mLastLocation.getAccuracy());
+                            } else {
+                                mLocationTextView.setText(R.string.no_location);
+                            }
+                        }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bag)));
+                    });
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
+        }
     }
-
-
 }
---
+++ app/src/main/java/com/example/tourguide/ExpandOrCollapse.java
@@ -0,0 +1,40 @@
+package com.example.tourguide;
+
+import android.animation.ValueAnimator;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+
+public class ExpandOrCollapse {
+
+    public static void expand(final View v, int duration, int targetHeight) {
+        int prevHeight  = v.getHeight();
+        v.setVisibility(View.VISIBLE);
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(prevHeight, targetHeight);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                v.getLayoutParams().height = (int) animation.getAnimatedValue();
+                v.requestLayout();
+            }
+        });
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.setDuration(duration);
+        valueAnimator.start();
+    }
+
+    public static void collapse(final View v, int duration, int targetHeight) {
+        int prevHeight  = v.getHeight();
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(prevHeight, targetHeight);
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                v.getLayoutParams().height = (int) animation.getAnimatedValue();
+                v.requestLayout();
+            }
+        });
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.setDuration(duration);
+        valueAnimator.start();
+    }
+}
